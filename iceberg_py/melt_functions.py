6e435629b52f95ec7ce218d503533b3c192d5837
shahinmg <shahinmg@ku.edu>
2023-08-02 12:11:52 -0500

    more melt functions

--- iceberg_py/melt_functions.py
+++ iceberg_py/melt_functions.py
@@ -10,14 +10,12 @@ import numpy as np
 import  scipy
 
 
-
 def melt_wave(windu, sst, sea_ice_conc):
     
     sea_state = 1.5 * np.sqrt(windu) + 0.1 * windu
     IceTerm = 1 + np.cos(np.power(sea_ice_conc,3) * np.pi)
     
     melt = (1/12) * sea_state * IceTerm * (sst + 2) # m/day
-    
     melt = melt / 86400 # m/second 
     
     return melt
@@ -34,3 +32,45 @@ def melt_solar(solar_rad):
     
     return melt
     
+
+def melt_forcedwater(temp_far, salinity_far, pressure_base, U_rel):
+    
+    # constants from Jenkins
+    a = -5.73e-2 # Salinity contribution
+    b = 8.32e-2 # constant
+    c = -7.61e-4 # pressure contribution C/dbar
+    
+    GT = 1.1e-3 # 6e-4; %econd value in Silva, first in Jenkins model heat transfer coefficient
+    GS = 3.1e-5 # 2.2e-5; %second value in Silva, first in Jenkins model % salt transfer coefficient
+    L = 3.35e5  # latent heat of fusion of ice (in J/kg)
+    cw = 3974;  # specific heat of water (J/kg/C)
+    ci = 2009;  # " of ice
+    DT = 15;    # temp difference between iceberg core and bottom surface (this is for Antarctica, maybe less for Greenland?)
+    
+    T_fp = a * salinity_far + b + c * pressure_base # Freezing point temperature
+    T_sh = temp_far - T_fp # Temperature above freezing point
+    
+    # Quadtratic Terms
+    A = (L + DT * ci) / (U_rel * GT * cw)
+    B = -((L + DT * ci)) *GS/(GT * cw) -a*salinity_far - T_sh
+    C = -U_rel * GS * T_sh
+    
+    ROOT = np.power(B,2) - 4*A*C
+    ROOT = np.where(ROOT<0,np.nan,ROOT)
+    
+    # Find quadratic roots
+    Mtemp1 = (-B + np.sqrt(ROOT)) / (2*A)
+    Mtemp2 = (-B - np.sqrt(ROOT)) / (2*A)
+    
+    melt = np.minimum(Mtemp1,Mtemp2)
+    
+    # clean data to remove melt rates below freezing point
+    mask = T_sh < 0
+    melt[mask] = 0
+    nan_mask = np.isnan(melt)
+    melt[nan_mask] = 0
+    
+    
+    return melt, T_sh, T_fp
+    
+    return
